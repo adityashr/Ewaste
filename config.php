1234f72739ffa511cab59c8bf81237b19487e7d8
Aditya <aditya31182005@gmail.com>
2026-05-15 16:56:00 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$db   = getenv('DB_NAME');
+$host = "sql204.infinityfree.com";
+$user = "if0_41927419";
+$pass = "aditya70177017";
+$db   = "if0_41927419_XXX";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
+
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("DB Error: " . mysqli_connect_error());
 }
 ?>
