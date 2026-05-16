c68ee1a031b493b4d0b1dd2a40195b65759e9f70
Aditya <aditya31182005@gmail.com>
2026-05-16 12:19:39 +0530

    first commit

--- config.php
+++ config.php
@@ -1,10 +1,5 @@
 <?php
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$db   = getenv('DB_NAME');
-
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect("localhost", "root", "", "ewaste_portal");
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
